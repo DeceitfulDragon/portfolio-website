a6fe5d9f0ecb2e3317038531e15a8ba6275179aa
CatGettingHitInTheFace <feartherenegade@gmail.com>
2024-05-12 04:08:46 -0500

    About page animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,16 +14,16 @@ $(document).ready(function () {
 
 
     // Anime.js animations
-    // Home
+    // Left side text in Home
     anime({
         targets: '.intro-content h1, .intro-content p, .intro-content ul, .intro-content .btn',
         translateX: [-500, 0],
         opacity: [0, 1],
-        delay: anime.stagger(100, {start: 300}),
+        delay: anime.stagger(100, { start: 300 }),
         duration: 1000,
         easing: 'easeOutExpo'
     });
-
+    // Right side profile image in Home
     anime({
         targets: '.profile-image',
         opacity: [0, 1],
@@ -32,5 +32,23 @@ $(document).ready(function () {
         delay: 1000
     });
 
+    // About Me Section in About
+    anime({
+        targets: '.about-section article p',
+        translateY: [20, 0],
+        opacity: [0, 1],
+        delay: anime.stagger(400),
+        easing: 'easeOutExpo',
+        duration: 1000
+    });
 
+    // Skills Grid List in About
+    anime({
+        targets: '.skills-grid .list-group-item',
+        translateX: [-30, 0],
+        opacity: [0, 1],
+        delay: anime.stagger(100, {start: 1000}),
+        easing: 'easeOutExpo',
+        duration: 800
+    });
 });
